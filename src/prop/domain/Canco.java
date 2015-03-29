1e499f641a3d4df916096dd17143441f364410fd
Oscar Mañas <oscar.manas@est.fib.upc.edu>
2015-03-29 19:34:23 +0200

    Mètodes afegits a la classe Canco (hòstia quin mal escriure això)

---
+++ src/prop/domain/Canco.java
@@ -0,0 +1,93 @@
+package prop.domain;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+/**
+ * A song
+ * @author Oscar Mañas Sanchez
+ */
+public class Canco {
+
+    private String title;
+    private String artist;
+    private String album;
+    private int year;
+    private String genre;
+    private String subgenre;
+    private int duration;
+    private ArrayList<Reproduccio> playbacks;
+
+    public Canco(String title, String artist, String album, int year, String genre, String subgenre, int duration) {
+        this.title = title;
+        this.artist = artist;
+        this.album = album;
+        this.year = year;
+        this.genre = genre;
+        this.subgenre = subgenre;
+        this.duration = duration;
+        playbacks = new ArrayList<Reproduccio>();
+    }
+
+    public String consultaTitol() {
+        return title;
+    }
+
+    public String consultaArtista() {
+        return artist;
+    }
+
+    public int consultaAny() {
+        return year;
+    }
+
+    public String consultaAlbum() {
+        return album;
+    }
+
+    public String consultaGenere() {
+        return genre;
+    }
+
+    public String consultaSubgenere() {
+        return subgenre;
+    }
+
+    public int consultaDurada() {
+        return duration;
+    }
+
+    public void modificaTitol(String title) {
+        this.title = title;
+    }
+
+    public void modificaArtista(String artist) {
+        this.artist = artist;
+    }
+
+    public void modificaAlbum(String album) {
+        this.album = album;
+    }
+
+    public void modificaAny(int year) {
+        this.year = year;
+    }
+
+    public void modificaGenere(String genre) {
+        this.genre = genre;
+    }
+
+    public void modificaSubgenere(String subgenre) {
+        this.subgenre = subgenre;
+    }
+
+    public void modificaDurada(int duration) {
+        this.duration = duration;
+    }
+
+    public void addPlayback(Usuari user, Calendar date) {
+        Reproduccio playback = new Reproduccio(user,this,date);
+        playbacks.add(playback);
+    }
+
+}
