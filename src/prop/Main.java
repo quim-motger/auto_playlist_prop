247c89ad26602f73e2183a6ced9510c7f2ffc8ef
Gerard Casas <gerard.casas.saez@est.fib.upc.edu>
2015-03-29 21:24:11 +0200

    Proposta de calcul edat al Main

--- src/prop/Main.java
+++ src/prop/Main.java
@@ -1,8 +1,23 @@
 package prop;
 
+import java.util.Calendar;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Rosa!");
+
+        Calendar dob = Calendar.getInstance();
+        dob.set(1995, 2, 6);
+        Calendar today = Calendar.getInstance();
+        int age = today.get(Calendar.YEAR) - dob.get(Calendar.YEAR);
+        if (today.get(Calendar.MONTH) < dob.get(Calendar.MONTH)) {
+            age--;
+        } else if (today.get(Calendar.MONTH) == dob.get(Calendar.MONTH)
+                && today.get(Calendar.DAY_OF_MONTH) < dob.get(Calendar.DAY_OF_MONTH)) {
+            age--;
+        }
+
+        System.out.println("Edat: " + age);
     }
 }
