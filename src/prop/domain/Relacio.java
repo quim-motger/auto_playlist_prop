839cd22e3611247534ef97c50435d9490d93d923
Gerard Casas <gerard.casas.saez@est.fib.upc.edu>
2015-03-30 11:41:38 +0200

    Classe Relació

---
+++ src/prop/domain/Relacio.java
@@ -0,0 +1,10 @@
+package prop.domain;
+
+/**
+ * Created by casassg on 30/03/15.
+ * @author casassg
+ * @version 1.0
+ */
+public abstract class Relacio {
+    public abstract boolean evalua(Canco c1, Canco c2);
+}
