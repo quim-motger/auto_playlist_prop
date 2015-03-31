2448041eed65f8a8f24cbd5707ab8b017f9a6f59
Gerard Casas <gerard.casas.saez@est.fib.upc.edu>
2015-04-01 01:53:40 +0200

    Canvis menors a List

--- src/prop/domain/List.java
+++ src/prop/domain/List.java
@@ -14,10 +14,29 @@ public class List {
     ArrayList<Song> songs;
 
     /**
-     * <code>List</code> creator method
+     * Creates an empty list with the designated title
      * @param listTitle   Title of the list
      */
-    public List(String listTitle) { title =listTitle;}
+    public List(String listTitle) { 
+        title = listTitle;
+        songs = new ArrayList<>();
+    }
+
+    /**
+     * <Creates a list with the designated title and the designated songs
+     * @param listTitle   Title of the list
+     * @param newSongs   Array of initial songs for the list
+     */
+    public List(String listTitle, ArrayList<Song> newSongs) { 
+        title = listTitle;
+        songs = newSongs;
+    }
+
+    /**
+     *  Obtain the number of songs in the list
+     * @return Number of songs in the list
+     */
+    public int size() { return songs.size();}
 
     /**
      * Obtain <code>List</code> title
@@ -26,19 +45,27 @@ public class List {
     public String obtainTitle() {return title;}
 
     /**
-     * Obtain the song at the position <code>pos</code>
-     * @param pos   <code>pos</code> is smaller than <code>obtainListSize()</code> and bigger than 0
-     * @return      <code>Song/code> of the list in the position <code>pos</code>
+     * Obtain the song at the position <code>position</code>
+     * @param position   <code>position</code> is smaller than <code>size()</code> and bigger than 0
+     * @return      <code>Song/code> of the list in the position <code>position</code>
      */
-    public Song obtainSong(int pos) {
-        return songs.get(pos);
+    public Song obtainSong(int position) {
+        return songs.get(position);
     }
-
+    
     /**
-     *  Obtain the number of songs in the list
-     * @return Number of songs in the list
+     * Obtains the position of a song inside the list
+     * @param songId Id of the searched song
+     * @return the position of the first time the song appears in the list or -1 if the song is not in the list
      */
-    public int obtainListSize() { return songs.size();}
+    public int obtainSongPosition(int songId) {
+        for(int i=0; i<songs.size(); ++i) {
+            if(songs.get(i).getId() == songId)
+                return i;
+            
+        }
+        return -1;
+    }
 
     /**
      * Obtain the addition of all the songs
@@ -46,11 +73,26 @@ public class List {
      */
     public int obtainTotalTime() {
         int total=0;
-        for (int i=0; i < obtainListSize();++i) {
+        for (int i=0; i < size();++i) {
             total += songs.get(i).getDuration();
         }
         return total;
     }
+
+    /**
+     *Check if a song is included in the list
+     * @param songId Id of the wanted song
+     * @return <code>true</code> if the song is in the list
+     */
+    public boolean checkSong(int songId) {
+        return obtainSongPosition(songId) != -1;
+    }
+
+    /**
+     * Checks if the List is empty 
+     * @return <code>true</code> if list is empty
+     */
+    public boolean isEmpty() {return size()==0;}
 
     /**
      *  Modifier of the List title
@@ -62,30 +104,47 @@ public class List {
 
     /**
      *  Modifier that adds song at the end of the <code>List</code>
-     *  <code>song</code> has an index of <code>obtainListSize()</code> - 1
+     *  <code>song</code> has an index of <code>size()</code> - 1
      *  @see Song
      */
     public void addSong(Song song) {
         songs.add(song);
     }
+    
+    /**
+     *  Modifier that adds songs at the end of the <code>List</code>
+     *  @param newSongs the order inside the array is the one desired to have in the list  
+     *  @see Song
+     */
+    public void addSongs(ArrayList<Song> newSongs) {
+        for(int i = 0;i<newSongs.size();++i) {
+            songs.add(newSongs.get(i));
+        }
+    }
 
     /**
      *  Modifier that deletes a song from the list
      *  The songs below the deleted one get promoted one position to fill the blank
-     * @param pos   0 < <code>pos</code> < <code>obtainListSize()</code>
+     * @param pos   0 < <code>pos</code> < <code>size()</code>
      */
     public void deleteSong(int pos) {
         songs.remove(pos);
     }
 
     /**
+     * Deletes all the songs from the list
+     * The List <code>isEmpty()</code> after this
+     */
+    public void empty() {songs = new ArrayList<>();}
+
+    /**
      * Modifier of order inside the list
      * Swaps two songs inside the list
-     * @param index1    0 < <code>index1</code> < <code>obtainListSize()</code>
-     * @param index2    0 < <code>index2</code> < <code>obtainListSize()</code>
+     * @param index1    0 < <code>index1</code> < <code>size()</code>
+     * @param index2    0 < <code>index2</code> < <code>size()</code>
      */
     public void swapSongs(int index1, int index2) {
-        Song song= songs.get(index1);
+        Song song = songs.get(index1);
         songs.set(index1, songs.get(index2));
         songs.set(index2, song);
     }
