b37361b4dfa68317dba222ade9cfb41057d2906f
Gerard Casas <gerard.casas.saez@est.fib.upc.edu>
2015-04-18 16:12:39 +0200

    ListDriver Done

--- src/prop/domain/ListDriver.java
+++ src/prop/domain/ListDriver.java
@@ -20,7 +20,10 @@ public class ListDriver {
         printInfo();
         
         List l=null;
+        Song song = null;
         Scanner in = new Scanner(System.in);
+        SongController sc = new SongController();
+        
         int i = -1;
         while (i!=0) {
             i = in.nextInt();
@@ -49,8 +52,8 @@ public class ListDriver {
                     break;
                 case 7:
                     ArrayList<Song> a = l.obtainSongs();
-                    for (Song song : a) {
-                        System.out.println(song.toString());
+                    for (Song s : a) {
+                        System.out.println(s.toString());
                     }
                     break;
                 case 8:
@@ -80,21 +83,6 @@ public class ListDriver {
                     l.editId(id);
                     break;
                 case 14:
-                    Song song = new Song();
-                    System.out.print("Title:");
-                    song.setTitle(in.next());
-                    System.out.print("Artist:");
-                    song.setArtist(in.next());
-                    System.out.print("Album:");
-                    song.setAlbum(in.next());
-                    System.out.print("Year:");
-                    song.setYear(in.nextInt());
-                    System.out.print("Genre:");
-                    song.setGenre(Genre.getGenreById(in.nextInt()));
-                    System.out.print("Subgenre:");
-                    song.setSubgenre(Genre.getGenreById(in.nextInt()));
-                    System.out.print("Duration:");
-                    song.setDuration(in.nextInt());
                     l.addSong(song);
                     break;
                 case 15:
@@ -112,9 +100,44 @@ public class ListDriver {
                     l.swapSongs(i1,i2);
                     break;
                 case 18:
-                    System.out.println(l.toString());                    
+                    System.out.println(l.toString());   
+                    break;
+                case 19:
+                    try {
+                        l = List.valueOf(in.next(),sc);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                        i=0;
+                    }
+                    break;
+                case 20:
+                    song = new Song(
+                            in.next(),
+                            in.next(), 
+                            in.next(),
+                            in.nextInt(),
+                            Genre.getGenreById(in.nextInt()),
+                            Genre.getGenreById(in.nextInt()),
+                            in.nextInt());
+                    break;
+                case 21:
+                    song = sc.getSong(in.next(),in.next());
+                    break;
+                case 22:
+                    try {
+                        sc.addSong(in.next(),
+                                in.next(),
+                                in.next(),
+                                in.nextInt(),
+                                Genre.getGenreById(in.nextInt()),
+                                Genre.getGenreById(in.nextInt()),
+                                in.nextInt());
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                        i=0;
+                    }
             }
-            System.out.println("END OF CONTENT");
+            
         }
     }
 
@@ -139,11 +162,19 @@ public class ListDriver {
         sb.append("16: void empty()\n");
         sb.append("17: void swapSongs(int index1, int index2)\n");
         sb.append("18: String toString()\n");
+        sb.append("19: valueOf(String origin, SongController sc)\n");
+        sb.append("20: song = new Song(String title, String artist, String album, int year, Genre genre, Genre subgenre, int duration)\n");
+        sb.append("21: song = sc.getSong(String title, String artist)\n");
+        sb.append("22: sc.addSong(String title, String artist, String album, int year, Genre genre, Genre subgenre, int duration)\n");
         sb.append("\n");
         System.out.print(sb.toString());
     }
+    
+    private static void printInfoBrief() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("0:  terminate program\n");
+        sb.append("1:  info\n");
+        System.out.println(sb.toString());
+        
+    }
 }
-
-/*
-TODO: Incorporar opcions driver de song al driver
- */
---
+++ src/prop/domain/Louvain.java
@@ -0,0 +1,11 @@
+package prop.domain;
+
+/**
+ * Louvain in prop.domain
+ *
+ * @author gerard.casas.saez
+ * @version 1.0
+ *          Creation Date: 18/04/15
+ */
+public class Louvain {
+}
