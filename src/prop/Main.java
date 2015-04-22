694f75f5ede56a57fdf83ec573feb33500751f94
Oscar Mañas <oscar.manas@est.fib.upc.edu>
2015-04-22 10:59:48 +0200

    Main corregit.

--- src/prop/Main.java
+++ src/prop/Main.java
@@ -22,7 +22,7 @@ public class Main {
 
         UserController cont = new UserController();
         try {
-            cont.addUser("test", "test", 3343434, "ES");
+            cont.addUser("test", "test", 3343434, 724); // 724 = ES
         } catch (Exception e) {
             e.printStackTrace();
         }
