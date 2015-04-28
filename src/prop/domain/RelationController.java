41cd3319145e0a7a6262917df7e6c4113afdebcb
joaquim.motger <quim.motger@gmail.com>
2015-04-28 18:41:16 +0200

    RelationController

---
+++ src/prop/domain/RelationController.java
@@ -0,0 +1,31 @@
+package prop.domain;
+
+import java.util.ArrayList;
+
+/**
+ * Relation Controller
+ * @author  gerard.casas.saez
+ * @author  joaquim.motger
+ */
+public class RelationController {
+
+    Graph graph;
+    boolean dirty;
+    ArrayList<Relation> relations;
+
+    public void createGraph() {
+
+    }
+
+    public Graph getGraph() {
+        return graph;
+    }
+
+    public void playbackRelations(UserController userController) {
+
+    }
+
+    public void addRelation(String s){
+
+    }
+}
--- src/prop/domain/SimpleRelation.java
+++ src/prop/domain/SimpleRelation.java
@@ -59,4 +59,6 @@ public class SimpleRelation extends Relation {
         }
     }
 
+
+
 }
