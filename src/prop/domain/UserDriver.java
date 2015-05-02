539f9da3e77dc662b68a61ca735bbd6ed3769835
Carles Garcia <gc.carles@gmail.com>
2015-05-02 13:55:08 +0200

    UserDriver acabat

--- src/prop/domain/UserDriver.java
+++ src/prop/domain/UserDriver.java
@@ -1,116 +1,159 @@
 package prop.domain;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.Scanner;
+import java.io.IOException;
+import java.util.*;
+
 /**
  * User Driver
  * @author Carles Garcia Cabot
  */
 public class UserDriver {
     public static void main(String[] args) {
-        System.out.println("**********************************************************");
-        System.out.println("** User");
-        System.out.println("**********************************************************");
-        System.out.print("\n");
-        printInfo();
+        try {
+            System.out.println("**********************************************************");
+            System.out.println("** User");
+            System.out.println("**********************************************************");
+            System.out.print("\n");
+            printInfo();
+            System.out.println("This class needs 5 other Classes: Playback, Song, SongController," +
+                    " List and List Controller\n"
+                    + "Therefore, you can create a Song, a Playback and a List");
 
-        User u = null;
-        Scanner in = new Scanner(System.in);
-        int i = -1;
-        while(i != 0) {
-            i = in.nextInt();
-            switch (i) {
-                case 0:
-                    break;
-                case 1:
-                    printInfo();
-                    break;
-                case 2:
-                    u = new User();
-                    break;
-                case 3: {
-                    String name = in.next();
-                    Gender gender = Gender.valueOf(in.next());
-                    System.out.println("Write day, month and year of birthdate separated by spaces");
-                    int day = in.nextInt();
-                    int month = in.nextInt();
-                    int y = in.nextInt();
-                    u.setBirthdate(new GregorianCalendar(y, month, day));
-                    u.setCountry(CountryCode.valueOf(in.next()));
-                    break;
+            User u = null;
+            Song song = null;
+            SongController songController = new SongController();
+            Playback playback = null;
+            List list = null;
+            ListController listController = new ListController();
+            Calendar date = Calendar.getInstance();
+            String serialized = "";
+            TreeSet<Playback> setPlayback = new TreeSet<>();
+            ArrayList<List> arrayList = new ArrayList<>();
+
+            Scanner in = new Scanner(System.in);
+            int i = -1;
+            while (i != 0) {
+                i = in.nextInt();
+                switch (i) {
+                    case 0:
+                        break;
+                    case 1:
+                        printInfo();
+                        break;
+                    case 2:
+                        u = new User();
+                        break;
+                    case 3: {
+                        u = new User(in.next(), Gender.valueOf(in.next()), new GregorianCalendar(in.nextInt(),in.nextInt(),in.nextInt()),
+                                CountryCode.valueOf(in.next()));
+                        break;
+                    }
+                    case 4:
+                        u = new User(in.next(), Gender.valueOf(in.next()), new GregorianCalendar(in.nextInt(),in.nextInt(),in.nextInt()),
+                                CountryCode.valueOf(in.next()), setPlayback, arrayList);
+                        break;
+                    case 5:
+                        u.add(playback);
+                        break;
+                    case 6:
+                        System.out.println(u.age());
+                        break;
+                    case 7:
+                        u.associate(list);
+                        break;
+                    case 8:
+                        u.clearLists();
+                        break;
+                    case 9:
+                        u.clearRegister();
+                        break;
+                    case 10:
+                        u.disassociate(list);
+                        break;
+                    case 11:
+                        ArrayList<List> al = u.getAssociatedLists();
+                        for (List l : al) System.out.println(l.toString());
+                        break;
+                    case 12:
+                        System.out.println(u.getBirthdate());
+                        break;
+                    case 13:
+                        System.out.println(u.getCountry());
+                        break;
+                    case 14:
+                        System.out.println(u.getGender());
+                        break;
+                    case 15:
+                        System.out.println(u.getName());
+                        break;
+                    case 16:
+                        TreeSet<Playback> ap = u.getPlaybackRegister();
+                        for (Playback p : ap) System.out.println(p);
+                        break;
+                    case 17:
+                        System.out.println(u.hasList(list));
+                        break;
+                    case 18:
+                        u.setAssociatedLists(arrayList);
+                        break;
+                    case 19:
+                        u.setBirthdate(date);
+                        break;
+                    case 20:
+                        u.setCountry(CountryCode.valueOf(in.next()));
+                        break;
+                    case 21:
+                        u.setGender(Gender.valueOf(in.next()));
+                        break;
+                    case 22:
+                        u.setName(in.next());
+                        break;
+                    case 23:
+                        u.setPlaybackRegister(setPlayback);
+                        break;
+                    case 24:
+                        serialized = u.toString();
+                        System.out.println(serialized);
+                        break;
+                    case 25:
+                        u = u.valueOf(serialized, listController, songController);
+                        break;
+                    case 26:
+                        playback = new Playback(song,date);
+                        break;
+                    case 27:
+                        song = songController.getSong(in.next(),in.next());
+                        break;
+                    case 28:
+                        songController.addSong(in.next(),in.next(),in.next(),in.nextInt(),Genre.valueOf(in.next()),
+                                Genre.valueOf(in.next()),in.nextInt());
+                        break;
+                    case 29:
+                        date.set(in.nextInt(),in.nextInt(),in.nextInt(),in.nextInt(),in.nextInt(),in.nextInt());
+                        break;
+                    case 30:
+                        list = new List(in.next());
+                        break;
+                    case 31:
+                        list.addSong(song);
+                        break;
+                    case 32:
+                        listController.addList(list);
+                        break;
+                    case 33:
+                        setPlayback.add(playback);
+                        break;
+                    case 34:
+                        arrayList.add(list);
+                        break;
+                    default:
+                        printInfo();
                 }
-                case 4:
-                    // TODO: User constructor with all members
-                    break;
-                case 5:
-                    // TODO: add playback
-                    break;
-                case 6:
-                    // TODO: associate list
-                    break;
-                case 7:
-                    u.clearLists();
-                    break;
-                case 8:
-                    u.clearRegister();
-                    break;
-                case 9:
-                    // TODO disassociate list
-                    break;
-                case 10:
-                    u.getAssociatedLists().toString();
-                    break;
-                case 11:
-                    u.getBirthdate().toString();
-                    break;
-                case 12:
-                    u.getCountry().getName().toString();
-                    break;
-                case 13:
-                    u.getGender().toString();
-                    break;
-                case 14:
-                    u.getName().toString();
-                    break;
-                case 15:
-                    u.getPlaybackRegister().toString();
-                    break;
-                case 16:
-                    // todo: hasList
-                    break;
-                case 17:
-                    // todo setAssociatedLists
-                    break;
-                case 18: {
-                    System.out.println("Write day, month and year of birthdate separated by spaces");
-                    int day = in.nextInt();
-                    int month = in.nextInt();
-                    int y = in.nextInt();
-                    u.setBirthdate(new GregorianCalendar(y, month, day));
-                    break;
-                }
-                case 19:
-                    u.setCountry(CountryCode.valueOf(in.next()));
-                    break;
-                case 20:
-                    u.setGender(Gender.valueOf(in.next()));
-                    break;
-                case 21:
-                    u.setName(in.next());
-                    break;
-                case 22:
-                    // todo setPlaybackREgister
-                    break;
-                case 23:
-                    System.out.println(u.toString());
-                    break;
-                // faltaria valueOf pero es molt dificil de testejar
-                default:
-                    printInfo();
+                printInfoBrief();
             }
-            printInfoBrief();
+        }
+        catch(Exception e) {
+            System.err.println("Caught Exception: " + e.getMessage());
         }
     }
 
@@ -142,9 +185,18 @@ public class UserDriver {
         sb.add("void 	setName(java.lang.String name)");
         sb.add("void 	setPlaybackRegister(java.util.TreeSet<Playback> playbackRegister)");
         sb.add("java.lang.String 	toString()");
-        sb.add(" static User 	valueOf(java.lang.String origin, ListController listController, SongController songController)");
+        sb.add("static User 	valueOf(java.lang.String origin, ListController listController, SongController songController)");
 
-
+        // OTHER CLASSES
+        sb.add("\nMETHODS FOR OTHER CLASSES\nplayback = new Playback(Song song, Calendar date)");
+        sb.add("song = songController.getSong(String title, String artist)");
+        sb.add("songController.addSong(String title, String artist, String album, int year, Genre genre, Genre subgenre, int duration)");
+        sb.add("date.set(int year, int month, int day, int hour, int minute, int second)");
+        sb.add("list = new List(String listTitle)");
+        sb.add("list.addSong(song)");
+        sb.add("listController.add(list)");
+        sb.add("setPlayback.add(playback)");
+        sb.add("arrayList.add(list)");
         for (int i = 0; i < sb.size(); ++i) {
             System.out.println(i + ": " + sb.get(i));
         }
