ceca676ee6f26d9f5799e1a92fe0f55ac0285e9f
Gerard Casas <gerard.casas.saez@est.fib.upc.edu>
2015-05-04 01:14:36 +0200

    Algorith IS READY TO GO!

--- src/prop/domain/Algorithm.java
+++ src/prop/domain/Algorithm.java
@@ -5,5 +5,12 @@ package prop.domain;
  *
  */
 public abstract class Algorithm {
+    /**
+     * Executes Community Detection Algorithm
+     *
+     * @param graph Input Graph
+     * @param k     Number of maximum communities
+     * @return Algorithm output with the steps of the algorithm and an array with the output communities
+     */
     public abstract AlgorithmOutput execute(Graph<Song> graph, int k);
 }
