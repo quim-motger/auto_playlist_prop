8569ab39a82cb8c3b3a59fa99c260e86eceb60d8
Gerard Casas <gerard.casas.saez@est.fib.upc.edu>
2015-05-22 19:42:25 +0200

    TabView updated

--- src/prop/presentation/TabView.java
+++ src/prop/presentation/TabView.java
@@ -8,9 +8,9 @@ import java.util.ArrayList;
  */
 public abstract class TabView extends JPanel {
 
+    private JScrollPane rightScrollPanel;
     private JPanel rightPanel;
     private JScrollPane leftListView;
-    private JList leftList;
     private JToolBar actionBar;
     private JTextField searchField;
 
@@ -21,29 +21,27 @@ public abstract class TabView extends JPanel {
         initComponents();
     }
 
-    public void setRightPanel(JPanel jPanel) {
-        rightPanel = jPanel;
-    }
-
-    public JList getLeftList() {
-        return leftList;
-    }
-
-    public JTextField getSearchField() {
-        return searchField;
-    }
-
-    public void initComponents() {
-        leftListView = new JScrollPane();
-        leftList = new JList();
+    protected void setLeftList(JList leftList) {
         leftListView.setViewportView(leftList);
+    }
+
+    protected void setRightPanel(JPanel rightPanel) {
+        rightScrollPanel.setViewportView(rightPanel);
+    }
+
+    private void initComponents() {
+        leftListView = new JScrollPane();
+        JList leftList = createLeftList();
+        leftListView.setViewportView(leftList);
+
+        rightScrollPanel = new JScrollPane();
+        JPanel rightPanel = createRightPanel();
+        rightScrollPanel.setViewportView(rightPanel);
 
         actionBar = new JToolBar();
         actionBar.setRollover(true);
         searchField = new JTextField();
         actionBar.add(searchField);
-
-        rightPanel = createRightPanel();
 
         ArrayList<JButton> buttons = setActionBarButtons();
         for (JButton button : buttons) {
@@ -55,24 +53,27 @@ public abstract class TabView extends JPanel {
         layout.setHorizontalGroup(
                 layout.createParallelGroup(GroupLayout.Alignment.LEADING)
                         .addGroup(layout.createSequentialGroup()
-                                .addComponent(leftListView, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                                .addComponent(leftListView, GroupLayout.PREFERRED_SIZE, 123, GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                                        .addComponent(rightPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                        .addComponent(actionBar, GroupLayout.DEFAULT_SIZE, 346, Short.MAX_VALUE)))
+                                .addComponent(rightScrollPanel, GroupLayout.DEFAULT_SIZE, 273, Short.MAX_VALUE))
+                        .addComponent(actionBar, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
                 layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                        .addComponent(leftListView, GroupLayout.DEFAULT_SIZE, 300, Short.MAX_VALUE)
                         .addGroup(layout.createSequentialGroup()
                                 .addComponent(actionBar, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(rightPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                .addGap(8, 8, 8)
+                                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                        .addComponent(leftListView, GroupLayout.DEFAULT_SIZE, 267, Short.MAX_VALUE)
+                                        .addComponent(rightScrollPanel)))
         );
     }
 
     protected abstract JPanel createRightPanel();
 
     protected abstract ArrayList<JButton> setActionBarButtons();
+
+    public abstract JList createLeftList();
+
 
 }
