c9d2e77d744a0f79365b91396641aca4c234ce9b
Carles Garcia <gc.carles@gmail.com>
2015-05-25 18:37:30 +0200

    Graph<Song> toString afegit i mètode de conversio a presentacio

--- src/prop/presentation/AlgorithmPController.java
+++ src/prop/presentation/AlgorithmPController.java
@@ -1,16 +1,29 @@
 package prop.presentation;
 
+import edu.uci.ics.jung.graph.UndirectedSparseGraph;
 import prop.domain.AlgorithmController;
 import prop.domain.RelationController;
+
 
 public class AlgorithmPController {
 
     private AlgorithmController algorithmController;
     private RelationController relationController;
-
+    private UndirectedSparseGraph<String,Double> gr;
+    static final String delimiter = "\n";
     public AlgorithmPController() {
         algorithmController = new AlgorithmController();
         relationController = new RelationController();
+        gr = new UndirectedSparseGraph<>();
+    }
+
+    public void createGraph(String graph) {
+        String[] vertices = graph.split(delimiter);
+        for (int i = 0; i < vertices.length; i += 3) {
+            gr.addVertex(vertices[i]);
+            gr.addVertex(vertices[i+1]);
+            gr.addEdge(Double.parseDouble(vertices[i+2]),vertices[i],vertices[i+1]);
+        }
     }
 
 }
