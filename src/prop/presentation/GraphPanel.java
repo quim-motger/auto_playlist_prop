750dea372cb524c08606a94bf28bae2affef259b
Carles Garcia <gc.carles@gmail.com>
2015-05-30 11:54:58 +0200

    GraphPanel

--- src/prop/presentation/GraphPanel.java
+++ src/prop/presentation/GraphPanel.java
@@ -3,7 +3,6 @@ package prop.presentation;
 import edu.uci.ics.jung.algorithms.layout.CircleLayout;
 import edu.uci.ics.jung.algorithms.layout.KKLayout;
 import edu.uci.ics.jung.algorithms.layout.Layout;
-import edu.uci.ics.jung.graph.DirectedSparseGraph;
 import edu.uci.ics.jung.graph.UndirectedSparseGraph;
 import edu.uci.ics.jung.graph.util.EdgeType;
 import edu.uci.ics.jung.visualization.GraphZoomScrollPane;
@@ -30,16 +29,25 @@ public class GraphPanel extends JPanel{
         private UndirectedSparseGraph<String, Double> graph;
         //the visual component and renderer for the graph
         private VisualizationViewer<String, Double> vv;
-
+        private int numberOfVertices = 20;
         public GraphPanel() {
             graph = new UndirectedSparseGraph<String, Double>();
-            String[] v = createVertices(50);
-            createEdges(v);
+            createVertices(numberOfVertices);
+            createEdges();
 
             Layout<String, Double> layout = new KKLayout<>(graph);
-            layout.setSize(new Dimension(800,600));
-            vv =  new VisualizationViewer<String, Double>(layout);
+            // layout size should scale with number of vertices
+            layout.setSize(new Dimension(3000,3000));
 
+            /*for (int i = 0; i < numberOfVertices; i++) {
+                layout.lock("V" + i, true);
+            }*/
+           /* for(String v : graph.getVertices()) {
+                transparency.put(v, new Double(0.9));
+            }
+*/
+            vv =  new VisualizationViewer<String, Double>(layout);
+            vv.setBackground(Color.white);
             vv.setPreferredSize(new Dimension(800, 600));
             Transformer<String, Paint> vertexColor = new Transformer<String, Paint>() {
                 public Paint transform(String i) {
@@ -49,7 +57,7 @@ public class GraphPanel extends JPanel{
             };
             Transformer<String, Shape> vertexSize = new Transformer<String, Shape>(){
                 public Shape transform(String i){
-                    Ellipse2D circle = new Ellipse2D.Double(-10, -10, 10, 10);
+                    Ellipse2D circle = new Ellipse2D.Double(-5,-5, 10, 10);
                     // in this case, the vertex is twice as large
                     //if(i == 2) return AffineTransform.getScaleInstance(2, 2).createTransformedShape(circle);
                     return circle;
@@ -69,16 +77,18 @@ public class GraphPanel extends JPanel{
                 }
             });
 
+            /** LABELS **/
             vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller<String>());
             vv.getRenderer().getVertexLabelRenderer().setPositioner(new InsidePositioner());
             vv.getRenderer().getVertexLabelRenderer().setPosition(Renderer.VertexLabel.Position.AUTO);
-            vv.setForeground(Color.lightGray);
+            vv.setForeground(Color.black); //label color
 
 
             this.setLayout(new BorderLayout(5, 5));
             final GraphZoomScrollPane panel = new GraphZoomScrollPane(vv);
             add(panel);
 
+            /** MOUSE AND BUTTONS **/
             final AbstractModalGraphMouse graphMouse = new DefaultModalGraphMouse<String,Double>();
             vv.setGraphMouse(graphMouse);
             vv.addKeyListener(graphMouse.getModeKeyListener());
@@ -114,26 +124,24 @@ public class GraphPanel extends JPanel{
             add(controls, BorderLayout.SOUTH);
         }
 
-        /**
-         * create some vertices
-         * @param count how many to create
-         * @return the Vertices in an array
-         */
-        private String[] createVertices(int count) {
-            String[] v = new String[count];
+        private void createVertices(int count) {
             for (int i = 0; i < count; i++) {
-                v[i] = "V"+i;
-                graph.addVertex(v[i]);
+                graph.addVertex("V"+i);
             }
-            return v;
+            graph.addVertex("Highway to hell (ACDC)");
         }
 
-        /**
-         * create edges for this demo graph
-         * @param v an array of Vertices to connect
-         */
-        void createEdges(String[] v) {
-            graph.addEdge(new Double(Math.random()), v[0], v[1]);
+        private void createEdges() {
+            graph.addEdge(10.5, "V0", "V1");
+            graph.addEdge(14.5, "V1", "V3");
+            graph.addEdge(313.5, "V6", "V3");
+            graph.addEdge(11.5, "V0", "V12");
+            graph.addEdge(9.5, "V15", "V9");
+            graph.addEdge(13.5, "V0", "V11");
+            graph.addEdge(1.5, "V5", "V1");
+            graph.addEdge(3.5, "V10", "V19");
+            graph.addEdge(121.5, "V6", "V8");
+            graph.addEdge(10.25, "V2", "V7");
         }
 
 }
